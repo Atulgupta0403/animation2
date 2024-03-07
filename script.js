f71ecd75d19b27eca5483a50e768d0e4e5772c91
Atulgupta0403 <atulgupta0403@gmail.com>
2024-03-07 11:52:03 +0530

    animation landing page

---
+++ script.js
@@ -0,0 +1,138 @@
+
+
+
+gsap.to(".image",{
+    width : "100%",
+    ease : Expo.easeInOut ,
+    duration : 3,
+    // repeat : -1,
+
+});
+
+gsap.to(".image",{
+    width : "0%",
+    ease : Expo.easeInOut,
+    duration : 3,
+    delay : 4,
+    // repeat : -1
+    
+});
+
+
+
+
+gsap.to(".image2",{
+    width : "60%",
+    ease : Expo.easeInOut ,
+    duration : 3,
+})
+
+gsap.to(".image2",{
+    width : "0%",
+    ease : Expo.easeInOut ,
+    duration : 3,
+    delay : 4
+
+})
+
+
+// gsap.to(".first",{
+//     width : "100%",
+//     ease : Expo.easeInOut ,
+//     duration : 3,
+
+// })
+
+
+// gsap.to(".first",{
+//     width : "0%",
+//     ease : Expo.easeInOut ,
+//     duration : 3,
+//     delay : 4
+
+// })
+
+
+gsap.to(".one",{
+    ease : Expo.easeInOut,
+    width : "100%",
+    duration : 2,
+    
+})
+
+gsap.to(".two",{
+    ease : Expo.easeInOut,
+    width : "100%",
+    duration : 2.3,
+    
+})
+
+gsap.to(".three",{
+    ease : Expo.easeInOut,
+    width : "100%",
+    duration : 2.6,
+    
+})
+
+gsap.to(".four",{
+    ease : Expo.easeInOut,
+    width : "100%",
+    duration : 3,
+    
+})
+
+
+gsap.to(".one",{
+    ease : Expo.easeInOut,
+    width : "0%",
+    duration : 2.6,
+    delay : 3,
+    
+})
+
+gsap.to(".two",{
+    ease : Expo.easeInOut,
+    width : "0%",
+    duration : 2.9,
+    delay : 3.3,
+
+    
+
+    
+})
+
+gsap.to(".three",{
+    ease : Expo.easeInOut,
+    width : "0%",
+    duration : 3.2,
+    delay : 3.6,
+
+    
+})
+
+gsap.to(".four",{
+    ease : Expo.easeInOut,
+    width : "0%",
+    duration : 3.5,
+    delay : 3.9,
+
+    
+})
+
+gsap.to(".vertical-text",{
+    // ease : Expo.easeInOut,
+    // width : "0%",
+    bottom : "10%",
+    duration : 4,
+    delay : 0.8
+})
+
+gsap.to(".vertical-text",{
+    // ease : Expo.easeInOut,
+    // width : "0%",
+    opacity : 0 ,
+    duration : 1.5,
+    delay : 5
+
+    
+})
